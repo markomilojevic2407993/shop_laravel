449ba182dfab5f89237039d4069cc304dd9a5ed9
Marko Milojevic <markomilojevic2407993.com>
2023-11-12 12:53:14 +0100

    convenc model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\HomeModel;
+
 class HomeController extends Controller
 {
     public function index()
     {
+        $products = HomeModel::all();
         $time = date('H');
 
-        return view('home', compact('time'));
+        return view('home', compact('time', 'products'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,4 +9,14 @@
 <p class="container text-center">Dobro vece{{$time}}</p>
 @endif
 
+@foreach($products as $product)
+<div class="container " style="border:1px solid black; width:320px; margin:4px;background-color:gray">
+<h2>{{$product->name}}</h2>
+<img src="{{$product->img}}" alt="" width="300px" height="300px" >
+<p>{{$product->description}}</p>
+<p>{{$product->amount}}</p>
+</div>
+@endforeach
+
+
 @endsection
